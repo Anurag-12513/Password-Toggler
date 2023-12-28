cc4e50c3302e577a87418267278a8484eb902c93
Anurag-12513 <anuragshandilya1122@gmail.com>
2023-12-28 20:27:15 +0530

    Adding some coments on js

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let getPbox= document.querySelector(".passwordBox");
 
 let eye = document.querySelector('#eye_icon');
 
-
+//Here making a function which will check if our password type is
+//password or text if it will be text then this function will chenge
+//change it to password when we'll click eye icon and vice-versa
 
  eye.onclick=function(){
     if(getPbox.type =="password"){
@@ -15,3 +17,4 @@ let eye = document.querySelector('#eye_icon');
         eye.src='eye-icons/eye-close.png';
     }
  }
+ 
